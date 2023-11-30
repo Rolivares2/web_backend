cba67b9c86b138c0af0e5e040f91e507da5e10a4
Re <rsteuber@uc.cl>
2023-11-29 23:20:50 -0300

    avanceee

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,28 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: web_pirates_db_user,
-    password: aVLDndeUXaZY3eFsjsEz2WzZritLTb3V,
-    database: web_pirates_db,
-    host: dpg-cljtna18mmjc73dcgua0-a,
-    dialect: 'postgres',
-  }
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME + "_test",
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
 };
-
-
--- src/models/index.js
+++ src/models/index.js
@@ -5,18 +5,24 @@ const Sequelize = require('sequelize');
 const process = require('process');
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config`)[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 
 const db = {};
 
 let sequelize;
-
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
+
+
 fs
   .readdirSync(__dirname)
   .filter((file) => (
